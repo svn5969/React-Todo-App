8dc934e984eb4d136c00422e6d9fbfe4b6e64bea
svn5969 <sourav.svn.10@gmail.com>
2021-11-03 01:42:59 +0600

    add items and delete items indivudual and all items work done

--- src/component/Todo.js
+++ src/component/Todo.js
@@ -1,33 +1,86 @@
-import React from 'react';
+import React, { useState } from 'react';
 import todo from "../component/images/todo.png";
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import "../App.css"
 
 const Todo = () => {
+    const [inputData, setInputData] = useState('')
+    const [items, setItems] = useState([])
+   
+    const addItem = () => {
+        if (!inputData) {
+            toast.error('Please Add the Item', {
+                position: "top-center"
+            })
+        }else {
+            setItems([...items, inputData])
+            setInputData('')
+        }
+
+    }
+    // enter key press
+    const handleKeypress = e => {
+        //it triggers by pressing the enter key
+      if (e.key === "Enter") {
+          console.log("enter press working");
+       addItem()
+      }
+    }; 
+    // delete items
+    const deleteItem=(id) => {
+     console.log(id);
+     const updatedItems = items.filter((elem,ind)=>{
+         return ind !== id
+     })
+     setItems(updatedItems)
+    }
+    // remove all items 
+
+    const removeAll=() => {
+        if (items) {
+            alert('Are You Sure ?')
+            setItems([])
+        }
+        
+    }
     return (
         <div className="main-div">
-         <div className="child-div">
-             <figure>
-                 <img src={todo} alt="" />
-                 <figcaption>Add Your List Here ✌️</figcaption>
-             </figure>
-             <div className="addItems">
-                 <input type="text" placeholder="✍ Add Items..." />
-                 <i className="fa fa-plus add-btn" title="Add Item"></i>
-             </div>
-             
-             <div className="showItems">
-                 <div className="eachItem">
-                     <h3>Apple</h3>
-                     <i className="fa fa-trash add-btn" title="Delete Item"></i>
-                 </div>
-             </div>
+            <div className="child-div">
+                <figure>
+                    <img src={todo} alt="" />
+                    <figcaption>Add Your List Here ✌️</figcaption>
+                </figure>
+                <div className="addItems">
+                    <input type="text" placeholder="✍ Add Items..."
+                        value={inputData} onChange={(e) => setInputData(e.target.value)}
+                        onKeyPress={handleKeypress}
+                    />
+                    <i className="fa fa-plus add-btn" title="Add Item" onClick={addItem}></i>
+                    <ToastContainer />
+                </div>
 
-             {/* clear all */}
-             <div className="showItems">
-                 <button className="btn effect04" data-sm-link-text="Remove All"><span>CHECK LIST</span></button>
-             </div>
+                <div className="showItems">
 
-         </div>
+                    {
+                        items.map((elem, ind) => {
+                            return (
+                                <div className="eachItem" key={ind}>
+                                    <h3>{elem}</h3>
+                                    <i className="fa fa-trash add-btn" title="Delete Item" onClick={()=>deleteItem(ind)}></i>
+                                </div>
+                            )
+                        })
+                    }
+
+                </div>
+
+                {/* clear all */}
+                <div className="showItems">
+                    <button className="btn effect04" data-sm-link-text="Remove All" onClick={removeAll}><span>CHECK LIST</span></button>
+                </div>
+
+            </div>
         </div>
     );
 };
